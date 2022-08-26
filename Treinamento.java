4225865e33f1a04b8985420d408ea5e874456ada
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-08-25 23:19:13 -0300

    Add files via upload

---
+++ Treinamento.java
@@ -0,0 +1,18 @@
+package br.com.fiap;
+
+	import java.util.Scanner;
+
+public class Treinamento 
+{
+	
+	public static void main(String[] arg) 
+	
+	{
+		
+		Scanner sc = new Scanner (System.in);
+		
+		
+		
+	}
+
+}
