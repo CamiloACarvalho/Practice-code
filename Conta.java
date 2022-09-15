495fe61ec7c5a5d009cb60fc0608db1f53584d3c
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-09-14 23:28:10 -0300

    Add files via upload

--- Conta.java
+++ Conta.java
@@ -4,13 +4,15 @@ public class Conta
 {
 // Uma classe pode não ter atributos, assim também como pode ter um ou vários atributos
 // Criamos dois atributos em comum, tanto para conta corrente, quanto para conta poupança...
-	
+
 	int numero; 
 	double saldo;
 	
 // Agora vou usar uma subclasse aqui. Para isso, basta eu declarar a classe cliente e dar um nome a ela
 	
 	Cliente cliente = new Cliente();
+
+		
+	}
 	
 	
-}
